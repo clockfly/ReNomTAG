06896cb0ceb53e782c8a54c4dc2856d8fa0b00a2
grid <s-kikuchi@gridsolar.jp>
2017-12-07 18:29:52 +0900

    process any page nation
    process any page nation number

--- src/pages/labeling_for_object_detection_page/file_list.vue
+++ src/pages/labeling_for_object_detection_page/file_list.vue
@@ -24,9 +24,13 @@
       >
       </file-item>
     </div>
+    {{ sidebar_page_nation }}
     <ul id="file-list-page-nation">
-      <li v-for="n in sidebar_page_number" @click="change_page(n)" :class="{active: n===sidebar_current_page}">{{ n }}
+      <li @click="change_page(1)" class="arrow"><<</li>
+      <li v-for="n in this.sidebar_page_nation" @click="change_page(n)" :class="{active: n===sidebar_current_page}">{{ n
+        }}
       </li>
+      <li @click="change_page(sidebar_page_number)" class="arrow">>></li>
     </ul>
   </div>
 </template>
@@ -41,7 +45,8 @@
     data: function () {
       return {
         // current_page: 1,
-        'select_page_step': 100
+        'select_page_step': 100,
+        'max_page': 5 // ページネーションの表示数
       }
     },
     created () {
@@ -66,6 +71,50 @@
       sidebar_page_number: function () {
         // divide file number by page step then plus1
         return parseInt(this.$store.getters.get_filename_list_length / this.sidebar_page_step) + 1
+      },
+      sidebar_page_nation: function () {
+        // divide file number by page step then plus1
+        let page_number = this.sidebar_page_number
+
+        // ページが6ページ以上ある場合
+        if (page_number > this.max_page) {
+          let temp_page_nation = []
+
+          // カレントページから最終ページまで6ページ以下である場合
+          if (page_number - this.sidebar_current_page < this.max_page - 1) {
+            for (let i = page_number - this.max_page + 1; i < page_number + 1; i++) {
+              temp_page_nation.push(i)
+            }
+            // カレントページから最終ページまで6ページ以上である場合
+          } else {
+            // カレントページが1ページ目である場合
+            if (this.sidebar_current_page === 1) {
+              for (let i = 1; i < this.max_page + 1; i++) {
+                temp_page_nation.push(i)
+              }
+              // カレントページが2ページ目である場合
+            } else if (this.sidebar_current_page === 2) {
+              console.log('2')
+              for (let i = this.sidebar_current_page - 1; i < this.sidebar_current_page + this.max_page - 1; i++) {
+                temp_page_nation.push(i)
+              }
+            } else if (this.sidebar_current_page === page_number - 1) {
+              for (let i = this.sidebar_current_page - (this.max_page - 2); i < page_number; i++) {
+                temp_page_nation.push(i)
+              }
+            } else {
+              for (let i = this.sidebar_current_page - 2; i < this.sidebar_current_page + this.max_page - 2; i++) {
+                temp_page_nation.push(i)
+              }
+              return temp_page_nation
+            }
+            // 最終ページ番号
+            // temp_page_nation.push(page_number)
+          }
+          return temp_page_nation
+        } else {
+          return page_number
+        }
       },
       sidebar_current_page: function () {
         return this.$store.getters.get_sidebar_current_page
@@ -138,7 +187,9 @@
       height: 10%;
 
       li {
-        padding: 2px 8px;
+        padding: 2px 0;
+        width: 25px;
+        text-align: center;
         border: 1px solid #0c0c0c;
         box-sizing: border-box;
         margin: 0 4px;
@@ -149,6 +200,9 @@
         }
         &:hover {
           cursor: pointer;
+        }
+        &.arrow {
+          letter-spacing: -1px;
         }
       }
     }
