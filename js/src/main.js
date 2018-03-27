c90b9e5f3ca160b6c7540dbd26edb4b3cf8c98f6
atsuo ishimoto <xatsuoi@gmail.com>
2018-03-27 16:47:54 +0900

    fontawesome should be loaded in prior to vue modules.

--- js/src/main.js
+++ js/src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from '@/App'
-import router from '@/router'
-import store from '@/store/store.js'
 
 require('@/css/reset.css');
 require('mini.css/dist/mini-default.min.css')
 require('font-awesome/css/font-awesome.css');
 require('@/css/common.css');
+
+import Vue from 'vue'
+import App from '@/App'
+import router from '@/router'
+import store from '@/store/store.js'
+
 
 Vue.config.productionTip = false
 
