c464f9b0f017f7ac5117155a26a749d195f0a72d
suwa <suwa@gridsolar.jp>
2018-10-10 09:39:57 +0900

    Modify docker file

--- docker/run.sh
+++ docker/run.sh
@@ -10,28 +10,21 @@ do
   case $OPT in
     "i" ) RUN_MODE="-it";;
     "d" ) FLG_A="TRUE" ; DATA_PATH=$(cd "$OPTARG" && pwd);;
-    "s" ) FLG_B="TRUE" ; STORAGE_PATH=$(cd "$OPTARG" && pwd);;
     "p" ) PORT="$OPTARG" ;;
   esac
 done
 
 if [ "$FLG_A" = "FALSE" ]; then
-    DATA_PATH=$(pwd)/datasrc
+    DATA_PATH=$(pwd)/public
     if [ ! -e $DATA_PATH ]; then
         mkdir -p $DATA_PATH
-    fi
-fi
-
-if [ "$FLG_B" = "FALSE" ]; then
-    STORAGE_PATH=$(pwd)/storage
-    if [ ! -e $STORAGE_PATH ]; then
-        mkdir -p $STORAGE_PATH
+        mkdir -p $DATA_PATH/user1/dataset
+        mkdir -p $DATA_PATH/user1/label
     fi
 fi
 
 echo "RUNNING Docker image"
 echo "PORT: " $PORT
-echo "DATA PATH: " $DATA_PATH
-echo "STORAGE PATH: " $STORAGE_PATH
+echo "PUBLIC DIR: " $DATA_PATH
 
-nvidia-docker run $RUN_MODE -p $PORT:8080 -v $DATA_PATH:/var/datasrc -v $STORAGE_PATH:/var/storage renom_img_docker
+nvidia-docker run $RUN_MODE -p $PORT:8080 -v $DATA_PATH:/var/public renom_tag_docker
